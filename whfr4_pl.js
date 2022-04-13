2ef3edc43d227071554a85a59c6058833a32eeca
Silent_Mark <marek.kembrowski@gmail.com>
2022-04-13 12:14:10 +0200

    update manifest and handle basic.db

--- whfr4_pl.js
+++ whfr4_pl.js
@@ -1,3 +1,9 @@
 Hooks.once("init", function() {
 	CONFIG.supportedLanguages["pl"] = "pl";
-})
+});
+Hooks.on("renderCompendiumDirectory", async () => {
+    if (game.packs.get("wfrp4e.basic")) {
+        game.packs.delete("wfrp4e.basic")
+        ui.sidebar.element.find("[data-pack='wfrp4e.basic']").remove()
+    }
+});
